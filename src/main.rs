d9c7e0482becfa3347cc725d95423d49148fc4df
Doug Goldstein <cardoe@cardoe.com>
2016-07-05 13:56:34 -0500

    initial version

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate cargo;
+extern crate rustc_serialize;
+
+use cargo::{Config, CliResult};
+use cargo::core::Package;
+use cargo::core::registry::PackageRegistry;
+use cargo::ops;
+use cargo::util::important_paths;
+
+#[derive(RustcDecodable)]
+struct Options {
+    flag_verbose: bool,
+    flag_quiet: bool,
+}
+
+fn main() {
+    cargo::execute_main_without_stdin(real_main, false, r#"
+Create BitBake recipe for a project
+
+Usage:
+    cargo bitbake [options]
+
+Options:
+    -h, --help          Print this message
+    -v, --verbose       Use verbose output
+    -q, --quiet         No output printed to stdout
+"#)
+}
+
+fn real_main(options: Options, config: &Config) -> CliResult<Option<()>> {
+    try!(config.shell().set_verbosity(options.flag_verbose, options.flag_quiet));
+
+    // Load the root package
+    let root = try!(important_paths::find_root_manifest_for_wd(None, config.cwd()));
+    let package = try!(Package::for_path(&root, config));
+
+    // Resolve all dependencies (generate or use Cargo.lock as necessary)
+    let mut registry = PackageRegistry::new(config);
+    try!(registry.add_sources(&[package.package_id().source_id().clone()]));
+    let resolve = try!(ops::resolve_pkg(&mut registry, &package, config));
+
+    for x in resolve.iter() {
+        println!("crate://{}/{}", x.name(), x.version());
+    }
+
+    Ok(None)
+}

3dd91c4cd96e17d2aab998378ba16482bb09eeba
Doug Goldstein <cardoe@cardoe.com>
2016-07-19 11:34:44 -0500

    Use rustache for populating the BitBake template
    Initial use of rustache to populate the BitBake template. Valid
    variables are:
    - summary
    - repository
    - index_commit
    - src_uri

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate cargo;
+extern crate rustache;
 extern crate rustc_serialize;
 
-use cargo::{Config, CliResult};
+use cargo::{Config, CliResult, CliError};
 use cargo::core::Package;
 use cargo::core::registry::PackageRegistry;
 use cargo::ops;
 use cargo::util::important_paths;
+use rustache::HashBuilder;
+use std::io::Read;
 
 #[derive(RustcDecodable)]
 struct Options {
@@ -41,8 +44,44 @@ fn real_main(options: Options, config: &Config) -> CliResult<Option<()>> {
     try!(registry.add_sources(&[package.package_id().source_id().clone()]));
     let resolve = try!(ops::resolve_pkg(&mut registry, &package, config));
 
+    // build the crate URIs
+    let mut src_uris = Vec::<String>::new();
     for x in resolve.iter() {
-        println!("crate://crates.io/{}/{}", x.name(), x.version());
+        src_uris.push(format!("crate://crates.io/{}/{} \\\n", x.name(), x.version()));
+    }
+    src_uris.push(String::from("crate-index://crates.io/CARGO_INDEX_COMMIT \\\n"));
+
+    // the bitbake recipe template
+    let template = include_str!("bitbake.template");
+
+    // root package metadata
+    let metadata = package.manifest().metadata();
+
+    // package description is used as BitBake summary
+    let summary = metadata.description
+        .as_ref()
+        .map(|t| t.clone())
+        .unwrap_or(String::from("unknown summary"));
+
+    // package repository
+    let repo = metadata.repository
+        .as_ref()
+        .map(|t| t.clone())
+        .unwrap_or(String::from("unknown repo"));
+
+    // build up the varibles for the template
+    let data = HashBuilder::new()
+        .insert_string("summary", summary.trim())
+        .insert_string("repository", repo.trim())
+        .insert_string("src_uri", src_uris.join(""));
+
+    match rustache::render_text(template, data) {
+        Ok(mut templ) => {
+            let mut mystr = String::new();
+            templ.read_to_string(&mut mystr);
+            println!("{}", mystr);
+        }
+        Err(_) => return Err(CliError::new("unable to generate BitBake recipe", 1)),
     }
 
     Ok(None)

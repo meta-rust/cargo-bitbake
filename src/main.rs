664b3633d83f79c0c815749192710bb13908b63d
Marcel Hellwig <github@cookiesoft.de>
2023-02-16 08:15:42 +0100

    replace structopt with clap

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@
 
 extern crate anyhow;
 extern crate cargo;
+extern crate clap;
 extern crate git2;
 extern crate itertools;
 extern crate lazy_static;
 extern crate md5;
 extern crate regex;
-extern crate structopt;
 
 use anyhow::{anyhow, Context as _};
 use cargo::core::registry::PackageRegistry;
@@ -26,14 +26,13 @@ use cargo::core::{Package, PackageSet, Resolve, Workspace};
 use cargo::ops;
 use cargo::util::{important_paths, CargoResult};
 use cargo::{CliResult, Config};
+use clap::Parser;
 use itertools::Itertools;
 use std::default::Default;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use structopt::clap::AppSettings;
-use structopt::StructOpt;
 
 mod git;
 mod license;
@@ -123,42 +122,42 @@ impl<'cfg> PackageInfo<'cfg> {
     }
 }
 
-#[derive(StructOpt, Debug)]
+#[derive(Parser, Debug)]
 struct Args {
     /// Silence all output
-    #[structopt(short = "q")]
+    #[arg(short)]
     quiet: bool,
 
     /// Verbose mode (-v, -vv, -vvv, etc.)
-    #[structopt(short = "v", parse(from_occurrences))]
-    verbose: usize,
+    #[arg(short, action = clap::ArgAction::Count)]
+    verbose: u8,
 
     /// Reproducible mode: Output exact git references for git projects
-    #[structopt(short = "R")]
+    #[arg(short = 'R')]
     reproducible: bool,
 
     /// Legacy Overrides: Use legacy override syntax
-    #[structopt(short = "l", long = "--legacy-overrides")]
+    #[arg(short, long)]
     legacy_overrides: bool,
 }
 
-#[derive(StructOpt, Debug)]
-#[structopt(
+#[derive(Parser, Debug)]
+#[command(
     name = "cargo-bitbake",
     bin_name = "cargo",
     author,
+    version,
     about = "Generates a BitBake recipe for a given Cargo project",
-    global_settings(&[AppSettings::ColoredHelp])
 )]
 enum Opt {
     /// Generates a BitBake recipe for a given Cargo project
-    #[structopt(name = "bitbake")]
+    #[command(name = "bitbake", version)]
     Bitbake(Args),
 }
 
 fn main() {
     let mut config = Config::default().unwrap();
-    let Opt::Bitbake(opt) = Opt::from_args();
+    let Opt::Bitbake(opt) = Opt::parse();
     let result = real_main(opt, &mut config);
     if let Err(e) = result {
         cargo::exit_with_error(e, &mut *config.shell());
